68fcd3ece017ec34e306e46054dee07ab366011c
eosmanspah2 <eosmanspah2@etf.unsa.ba>
2024-01-25 15:51:11 +0100

    fixed issue with repo inside repo

---
+++ crud-test/app/Models/Player.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Player extends Model
+{
+    use HasFactory;
+    //add name and email to fillable fields
+    protected $fillable = ['name', 'email'];
+}
